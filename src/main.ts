bba8771cefda5b446cf4de4197b1a051e3302c81
wywang <727936199@qq.com>
2020-11-08 17:19:47 +0800

    封装router.ts

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,8 @@
 import { createApp } from 'vue'
 import './index.scss'
 import App from './App.vue'
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import Switch from './components/Switch.vue'
-import Button from './components/Button.vue'
-import Dialog from './components/Dialog.vue'
-import Tabs from './components/Tabs.vue'
-import {createWebHistory,createRouter} from 'vue-router'
+import {router} from './router'
 
-const history=createWebHistory();
-const router=createRouter({
-    history:history,
-    routes:[
-        {
-            path:'/',
-            component:Home
-        },
-        {
-            path:'/doc',
-            component:Doc,
-            children:[
-                {
-                    path:'/doc/switch',
-                    component:Switch
-                },
-                {
-                    path:'/doc/button',
-                    component:Button
-                },
-                {
-                    path:'/doc/dialog',
-                    component:Dialog
-                },
-                {
-                    path:'/doc/tabs',
-                    component:Tabs
-                },
-            ]
-        }
-    ]
-});
 const app=createApp(App);
 app.use(router);
 app.mount('#app');
---
+++ src/router.ts
@@ -0,0 +1,42 @@
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
+import Switch from './components/Switch.vue'
+import Button from './components/Button.vue'
+import Dialog from './components/Dialog.vue'
+import Tabs from './components/Tabs.vue'
+import {createWebHistory,createRouter} from 'vue-router'
+
+const history=createWebHistory();
+const router=createRouter({
+    history:history,
+    routes:[
+        {
+            path:'/',
+            component:Home
+        },
+        {
+            path:'/doc',
+            component:Doc,
+            children:[
+                {
+                    path:'switch',
+                    component:Switch
+                },
+                {
+                    path:'button',
+                    component:Button
+                },
+                {
+                    path:'dialog',
+                    component:Dialog
+                },
+                {
+                    path:'tabs',
+                    component:Tabs
+                },
+            ]
+        }
+    ]
+});
+
+export {router};
