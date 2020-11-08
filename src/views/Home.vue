78f4b90547ef723ea3e403aa793d5117d37ee8cc
wywang <727936199@qq.com>
2020-11-08 14:14:51 +0800

    完成Home组件样式布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,47 @@
 <template>
-    <div class="topnav">
-        <div class="logo"></div>
-        <div class="menu"></div>
+    <TopNav/>
+    <div class="content">
+        <h3>一个简单易上手的UI框架</h3>
+        <div class="link">
+            <a class="git">Github</a>
+            <a class="get">Get Started</a>
+        </div>
     </div>
-    <div>一个简单易上手的UI框架</div>
-    <a>github</a>
-    <a>get started</a>
 </template>
 
 <script>
+    import TopNav from '../components/TopNav.vue';
     export default {
-        name:'Home'
+        name:'Home',
+        components: {TopNav}
     };
 </script>
+
+<style lang="scss" scoped>
+    .content{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        >h3{
+            margin: 20px 0;
+        }
+        >.link{
+            display: flex;
+            align-items: center;
+            >a{
+                margin: 5px;
+                padding: 8px;
+                border-radius: 16px;
+                &.git{
+                    background: #7480a3;
+                    color: white;
+                }
+                &.get{
+                    border:1px solid #7480a3;
+                    color: #7480a3;
+                }
+            }
+        }
+    }
+</style>
 
