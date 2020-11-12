aebab3b99947554707a66c99ae10330036182e8e
wywang <727936199@qq.com>
2020-11-12 19:16:43 +0800

    完成Dialog样式

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="moon-overlay"></div>
-    <div class="moon-dialog">
-        <header>标题</header>
-        <main>
-            <p>第一行</p>
-            <p>第二行</p>
-        </main>
-        <footer>
-            <m-button>取消</m-button>
-            <m-button>确认</m-button>
-        </footer>
+    <div class="moon-dialog-overlay"></div>
+    <div class="moon-dialog-wrapper">
+        <div class="moon-dialog">
+            <header>标题<span class="moon-dialog-close"></span></header>
+            <main>
+                <p>第一行</p>
+                <p>第二行</p>
+            </main>
+            <footer>
+                <m-button level="main">确认</m-button>
+                <m-button>取消</m-button>
+            </footer>
+        </div>
     </div>
 </template>
 
@@ -24,5 +26,69 @@
 </script>
 
 <style lang="scss">
+    $radius: 4px;
+    $border-color: #d9d9d9;
+    .moon-dialog {
+        background: white;
+        border-radius: $radius;
+        box-shadow: 0 0 3px fade_out(black, 0.5);
+        min-width: 15em;
+        max-width: 90%;
+        &-overlay {
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: fade_out(black, 0.5);
+            z-index: 10;
+        }
+        &-wrapper {
+            position: fixed;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            z-index: 11;
+        }
+        >header {
+            padding: 12px 16px;
+            border-bottom: 1px solid $border-color;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            font-size: 20px;
+        }
+        >main {
+            padding: 12px 16px;
+        }
+        >footer {
+            border-top: 1px solid $border-color;
+            padding: 12px 16px;
+            text-align: right;
+        }
+        &-close {
+            position: relative;
+            display: inline-block;
+            width: 16px;
+            height: 16px;
+            cursor: pointer;
+            &::before,
+            &::after {
+                content: '';
+                position: absolute;
+                height: 1px;
+                background: black;
+                width: 100%;
+                top: 50%;
+                left: 50%;
+            }
+            &::before {
+                transform: translate(-50%, -50%) rotate(-45deg);
+            }
+            &::after {
+                transform: translate(-50%, -50%) rotate(45deg);
+            }
+        }
+    }
 
 </style>
