af1d7020330ff9102e5141e14b2af6e0d22859ac
wywang <727936199@qq.com>
2020-11-18 18:05:25 +0800

    完成DialogDemo和TabsDemo组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,38 +1,22 @@
 <template>
-    <m-dialog :visible="visible" @update:visible="visible=$event"
-              :closeOnClickOverlay="closeOnClickOverlay" :ok="f1" :cancel="f2">
-        <template v-slot:title>
-            <p>真的吗？</p>
-        </template>
-        <template v-slot:content>
-            <p>我想吃芋圆了</p>
-        </template>
-    </m-dialog>
-    <m-button @click="visible=!visible">toggle</m-button>
+    <h1>Dialog 示例</h1>
+    <Demo :component="Dialog1Demo" />
+    <Demo :component="Dialog2Demo" />
 </template>
 
 <script lang="ts">
-    import Dialog from '../lib/Dialog.vue'
-    import Button from '../lib/Button.vue'
-    import {ref} from 'vue'
+    import Demo from './Demo.vue';
+    import Dialog1Demo from './Dialog1.demo.vue';
+    import Dialog2Demo from './Dialog2.demo.vue';
     export default {
         name:'MoonDialogDemo',
         components:{
-            'm-dialog':Dialog,
-            'm-button':Button
+            Demo
         },
         setup(){
-            const visible=ref(false);
-            const closeOnClickOverlay=ref(false);
-            const title=ref('真的吗');
-            const content=ref('薛定谔的猫');
-            const f1=()=>{
-                return false;
-            }
-            const f2=()=>{
-                return true;
-            }
-            return {visible,closeOnClickOverlay,f1,f2,title,content};
+
+            return {Dialog1Demo,
+                Dialog2Demo};
         }
     };
 </script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,24 +1,18 @@
 <template>
-    <div>tabs</div>
-    <m-tabs v-model:selected="selected">
-        <m-tab title="标题一">内容一</m-tab>
-        <m-tab title="标题二">内容二</m-tab>
-    </m-tabs>
+    <h1>Tabs 示例</h1>
+    <Demo :component="Tabs1Demo" />
 </template>
 
 <script lang="ts">
-    import Tabs from '../lib/Tabs.vue';
-    import Tab from '../lib/Tab.vue';
-    import {ref} from 'vue'
+    import Demo from './Demo.vue'
+    import Tabs1Demo from './Tabs1.demo.vue'
     export default {
         name:'MoonTabsDemo',
         setup(){
-            const selected=ref('标题二');
-            return {selected}
+            return {Tabs1Demo}
         },
         components: {
-            'm-tab':Tab,
-            'm-tabs':Tabs
+            Demo
         }
     };
 </script>
